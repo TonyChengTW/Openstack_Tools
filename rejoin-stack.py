e3e11bf6e8cec1eca1bddb7a979e2a8de5143904
Tony Cheng <tony.pig@gmail.com>
2016-03-22 13:40:17 +0800

    my tools

--- rejoin-stack.py
+++ rejoin-stack.py
@@ -1,19 +1,19 @@
 #!/usr/bin/python
 # Edit by Tony
 # Replace rejoin-stack.sh
-# Version v0.1
+# Version = 0.1
 
 import time
 import commands
 
 
 os_services = {'nova-api': '/usr/local/bin/nova-api',
-               'nova-compute': 'sg libvirtd /usr/local/bin/nova-compute --config-file /etc/nova/nova.conf'}
-
+               'nova-compute': 'sg libvirtd /usr/local/bin/nova-compute --config-file /etc/nova/nova.conf',
+              }
 for service_key in os_services:
-    print '\n\n+++++++++++++ starting %s : %s++++++++++++++++++' % service_key, os_services[service_key]
-    time.sleep(2)
-    cli = 'screen -t os_services[service_key] -s os_services[service_key]'
+    print '\n\n+++++++++++++ Starting %s : %s  ++++++++++++++++++' % (service_key, os_services[service_key])
+    #time.sleep(2)
+    cli = 'sudo screen -S "Openstack services" -t %s -s "%s"' % (service_key, os_services[service_key])
+    print cli
     cmd_result = commands.getstatusoutput(cli)
     print cmd_result
-
