f540d8fb308b8d8b445ed4b0c69da4cab18ab538
Tony Cheng <tony.pig@gmail.com>
2016-05-19 15:13:24 +0800

    add subprocess and comment-out commands.getstatus

--- purge_tokens.py
+++ purge_tokens.py
@@ -4,7 +4,8 @@
 # Version = 0.1
 
 import time
-import commands
+import subprocess
+#import commands
 
 
 shrink_innodb_cmd = 'mysql -h 192.168.2.10 -u root -pAbc12345 keystone -e "alter table token ENGINE=\'InnoDB\'"'
@@ -18,6 +19,9 @@ for purge_cmd in tokens_purge:
     print '\n\n+++++++ Starting %s : %s  ++++++++++++' % (purge_cmd, tokens_purge[purge_cmd])
     time.sleep(2)
     cli = tokens_purge[purge_cmd]
-    print cli
-    cmd_result = commands.getstatusoutput(cli)
-    print cmd_result
+    print "Command = %s" % cli
+    cmd_result = subprocess.call(cli,
+                                 shell=True)
+#    cmd_result = commands.getstatusoutput(cli)
+
+    print "Result = %s" % cmd_result
